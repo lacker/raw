3b1cc90f1e0d43d92518bd682e703e323c5f991a
Kevin Lacker <lacker@gmail.com>
2022-05-19 11:38:06 -0700

    documenting the raw header format

--- header.h
+++ header.h
@@ -2,26 +2,90 @@
 
 namespace raw {
 
+  /*
+    The Header contains the information we get from processing one block of the .raw file.
+
+   The .raw file format is similar to the FITS format:
+     https://fits.gsfc.nasa.gov/fits_primer.html
+  
+   However, the header does not include the fields that are required in FITS, and it does not store the data blocks
+   in any of the ways that are supported by the FITS format, so you generally cannot expect to use libraries designed to read FITS
+   files on a .raw file. The headers are formatted the same way, though, minus the missing required fields, so we can use some code
+   originally written to parse FITS headers.
+   
+   Most of the fields of the Header map directly to FITS headers. Some of them are auxiliary information
+   generated during the parsing process.
+  */
   typedef struct {
     int directio;
+
+    // The "BLOCSIZE" FITS header.
+    // This is the size of the following data segment, in bytes, not including directio padding.
     size_t blocsize;
+
+    // The "NPOL" FITS header, with the exception that any 4 is treated like a 2, for reasons that are lost in time.
+    // This is the number of polarities for the data.
     unsigned int npol;
+
+    // The "OBSNCHAN" FITS header.
+    // This is the number of frequency channels for the data.
     unsigned int obsnchan;
+
+    // The "NBITS" FITS header.
+    // This is the number of bits used to store each real or complex values, as a signed int.
+    // We only support nbits = 8.
     unsigned int nbits;
-    int64_t pktidx; // TODO make uint64_t?
+
+    // The "PKTIDX" FITS header.
+    // This is an index that counts up through the file, to let us detect missing blocks.
+    int64_t pktidx;
+
+    // The "OBSFREQ" FITS header.
+    // This is the center frequency of the entire range of frequencies stored in the file.
+    // TODO: what units is this in?
     double obsfreq;
+
+    // The "OBSBW" FITS header.
+    // This is the width of the range of frequencies. Negative indicates a reversed frequency axis.
+    // TODO: what units is this in?
     double obsbw;
+
+    // The "TBIN" FITS header.
+    // This is the time resolution for the data.
+    // TODO: what units is this in?
     double tbin;
-    double ra;  // hours
-    double dec; // degrees
+
+    // The right ascension of the telescope.
+    // TODO: what units - hours? degrees?
+    // This is derived from the "RA_STR" FITS header.
+    double ra;
+
+    // The declination of the telescope.
+    // TODO: what units - degrees?
+    // This is derived from the "DEC_STR" FITS header.
+    double dec;
+
+    // The start time in MJD format.
+    // This is synthesized from the "STT_IMJD" and "STT_SMJD" FITS headers.
     double mjd;
+
+    // TODO: keep documenting
     int beam_id; // -1 is unknown or single beam receiver
     int nbeam;   // -1 is unknown or single beam receiver
     unsigned int nants;
     char src_name[81];
     char telescop[81];
     off_t hdr_pos; // Offset of start of header
+
+    // The "HDR_SIZE" FITS header.
     size_t hdr_size; // Size of header in bytes (not including DIRECTIO padding)
+
+    // Normally, pktidx increases by 1 each block.
+    // In some cases, the process writing the .raw file doesn't write some of the blocks that it would normally.
+    // missing_blocks provides the number of blocks that were "missing" in this way, between this header and the previous block.
+    // Depending on the downstream application, you might want to treat these blocks as being equivalent to all zeros, or
+    // simply ignore them.
+    int missing_blocks;
   } Header;
 
 }
